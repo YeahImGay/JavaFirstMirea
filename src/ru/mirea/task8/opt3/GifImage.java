a7b8b46120703622b1d2030d677aae19a429f778
YeahImGay <sfh200-2002@mail.ru>
2021-10-27 13:44:02 +0300

    Task 8

---
+++ src/ru/mirea/task8/opt3/GifImage.java
@@ -0,0 +1,22 @@
+package ru.mirea.task8.opt3;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class GifImage {
+    public static void main(String[] args){
+        String path = args[0];
+        JFrame frame = new JFrame("Image");
+        frame.setSize(600, 338);
+        Image image =Toolkit.getDefaultToolkit().createImage(path);
+        JLabel label =new JLabel("");
+        ImageIcon imageIcon= new ImageIcon(image);
+        imageIcon.setImageObserver(label);
+        label.setIcon(imageIcon);
+        frame.add(label);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
