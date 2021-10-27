42c0f3e88bba14ba304da6a9d918855cb72db155
YeahImGay <sfh200-2002@mail.ru>
2021-10-27 13:50:21 +0300

    Task 8

--- src/ru/mirea/task8/opt3/GifImage.java
+++ src/ru/mirea/task8/opt3/GifImage.java
@@ -8,7 +8,7 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class GifImage {
     public static void main(String[] args){
         String path = args[0];
-        JFrame frame = new JFrame("Image");
+        JFrame frame = new JFrame("Gif");
         frame.setSize(600, 338);
         Image image =Toolkit.getDefaultToolkit().createImage(path);
         JLabel label =new JLabel("");
